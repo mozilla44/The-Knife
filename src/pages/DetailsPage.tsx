5e663effccebb185fb64171b33b906d2080e993a
antoine <antoine.frisco@gmail.com>
2023-06-12 17:01:39 +0200

    css details done

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -3,7 +3,7 @@ import { Restaurant } from "../models/Restaurant";
 import { useEffect, useState, useContext } from "react";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import { NotFound } from "./NotFound";
-import "./detailsPage.css"
+import "./detailsPage.css";
 
 export const DetailsPage = () => {
   const { id } = useParams();
@@ -14,26 +14,38 @@ export const DetailsPage = () => {
     if (id != null) {
       const result = restaurants.find((r) => r.id == +id);
       console.log(result);
-      if (result!= null) {
-      setResto(result);
-    }}
-   
+      if (result != null) {
+        setResto(result);
+      }
+    }
   }, [id, restaurants]);
 
-  if (resto == null) return (<NotFound/>)
-
+  if (resto == null) return <NotFound />;
 
   return (
-    <div className="details_wrapper">
+   <div className="details_page">
       <img src={resto.img} className="details_img"></img>
-      <div className="details_intro">
+      <div className="details_wrapper">
+        <section className="left_pannel">
+        <div className="details_intro">
       <h1 className="details_name">{resto.name}</h1>
       <button className="add_me" onClick={()=>{console.log("coucou")}}>Save as favorite</button>
       </div>
-      {/* <h1 className="details_name">{resto.name}<button>Save as favorite</button></h1> */}
       <h2 className="details_adress" >{resto.address}</h2>
       <p className="details_description">{resto.description_long}</p>
-      
-    </div>
+        </section>
+        <section className="right_pannel">
+          <h2 className="details_menu">Menu:</h2>
+          <div className="entrees"><h3 className="menu_cat_name">Entrees:</h3>{resto.menu.entrees.map(e => <p className="item">{e}</p>)}</div>
+          <div className="dishes"><h3 className="menu_cat_name">Dishes:</h3>{resto.menu.dishes.map(e => <p className="item">{e}</p>)}</div>
+          <div className="desert"><h3 className="menu_cat_name">Deserts:</h3>{resto.menu.deserts.map(e => <p className="item">{e}</p>)}</div>
+
+
+        </section>
+      </div>
+    
+   </div>
   );
 };
+
+  
