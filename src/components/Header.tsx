d10ca0d89b7027ee86c0d71d8153414fc379d073
antoine <antoine.frisco@gmail.com>
2023-06-12 17:15:11 +0200

    css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,9 @@ export const Header = () => {
       <Link to="/" className="logo_link">
         <img className="logo" alt="logo" src="/src/assets/logo.png"></img>
       </Link>
+      <Link to="/" className="title_link">
+      <h1 className="app_title">The Knife</h1>
+      </Link>
       <nav className="topnav">
       <Link to="/" className="homepage_link">Home</Link>
       <Link to="/favorites" className="favorites_link">Favorites</Link>
