77f4ce7c69451c5d3271bdcf3c03d56fbfa67c44
antoine <antoine.frisco@gmail.com>
2023-06-12 16:20:33 +0200

    wip css

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -3,6 +3,7 @@ import { Restaurant } from "../models/Restaurant";
 import { useEffect, useState, useContext } from "react";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import { NotFound } from "./NotFound";
+import "./detailsPage.css"
 
 export const DetailsPage = () => {
   const { id } = useParams();
@@ -23,11 +24,16 @@ export const DetailsPage = () => {
 
 
   return (
-    <div>
-      <h1>{resto.name}</h1>
-      <h1>{resto.address}</h1>
-      <h1>{resto.description_long}</h1>
-      <img src={resto.img}></img>
+    <div className="details_wrapper">
+      <img src={resto.img} className="details_img"></img>
+      <div className="details_intro">
+      <h1 className="details_name">{resto.name}</h1>
+      <button className="add_me" onClick={()=>{console.log("coucou")}}>Save as favorite</button>
+      </div>
+      {/* <h1 className="details_name">{resto.name}<button>Save as favorite</button></h1> */}
+      <h2 className="details_adress" >{resto.address}</h2>
+      <p className="details_description">{resto.description_long}</p>
+      
     </div>
   );
 };
